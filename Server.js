527c38885321877f2275a701cf0b351f6f608afb
ALANGAR VAISHAK <vaishakalangar123@gmail.com>
2024-02-09 23:36:13 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
